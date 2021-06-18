26d52d6c98ac7bd9ddc5aaba04ebc10b086554cd
Sky <sky9413@gmail.com>
2021-06-19 02:15:45 +0800

    start up

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { RichEditor, RichToolbar, actions, defaultActions, createHTML, getContentCSS } from './editor/index'
 
 export default function App() {
+
+  const [richText, setrichText] = useState();
+  const [isrichtextLoaded, setisrichtextLoaded] = useState();
+  // const defaultActions = [
+  //   actions.insertImage,
+  //   actions.setBold,
+  //   actions.setItalic,
+  //   actions.insertBulletsList,
+  //   actions.insertOrderedList,
+  //   actions.insertLink
+  // ];
+
+  const onEditorInitialized = () => {
+    setisrichtextLoaded(true)
+  }
+  console.log(isrichtextLoaded);
+  useEffect(() => {
+    onEditorInitialized();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 99 }}>
+      <Text>___________________Editor Start_________________</Text>
+      <RichEditor
+        ref={(r) => richText == r}
+        initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
+
+        editorInitializedCallback={() => onEditorInitialized()}
+      />
+      {isrichtextLoaded === true
+        ? <RichToolbar
+          getEditor={() => richText} />
+        : null
+      }
+
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
